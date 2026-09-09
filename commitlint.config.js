0ac4cbc5e5b243d96b2f96b8aa2d1a3a1ccef77e
eduardo-hdez <ehdez2004@gmail.com>
2026-09-09 08:42:23 -0600

    style(formatting): update code indentation

--- commitlint.config.js
+++ commitlint.config.js
@@ -1,23 +1,23 @@
 export default {
-    extends: ["@commitlint/config-conventional"],
-    rules: {
-        "type-enum": [
-            2,
-            "always",
-            [
-                "feat",
-                "fix",
-                "chore",
-                "docs",
-                "style",
-                "refactor",
-                "perf",
-                "test",
-                "build",
-                "ci",
-                "revert",
-            ],
-        ],
-        "subject-case": [0], // allow any case for subject
-    },
+  extends: ["@commitlint/config-conventional"],
+  rules: {
+    "type-enum": [
+      2,
+      "always",
+      [
+        "feat",
+        "fix",
+        "chore",
+        "docs",
+        "style",
+        "refactor",
+        "perf",
+        "test",
+        "build",
+        "ci",
+        "revert",
+      ],
+    ],
+    "subject-case": [0], // allow any case for subject
+  },
 };
