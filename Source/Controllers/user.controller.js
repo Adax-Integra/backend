4b1134f6688ae298d93443db518caefed31bf105
eduardo-hdez <ehdez2004@gmail.com>
2026-09-11 07:38:07 -0600

    feat(r-01): add editPreSubmissionData method in user controller

--- Source/Controllers/user.controller.js
+++ Source/Controllers/user.controller.js
@@ -1,17 +1,41 @@
 import CheckPreSubmissionDataUseCase from '../../Domain/UseCases/checkPreSubmissionData.usecase.js';
+import EditPreSubmissionDataUseCase from '../../Domain/UseCases/editPreSubmissionData.usecase.js';
 
+// Instantiate use case
 const checkPreSubmissionDataUseCase = new CheckPreSubmissionDataUseCase();
+const editPreSubmissionDataUseCase = new EditPreSubmissionDataUseCase();
 
 class UserController {
   async getPreSubmissionData(req, res) {
     try {
       const { userId } = req.params;
-
       const profileData = await checkPreSubmissionDataUseCase.execute(userId);
 
       return res.status(200).json({
         success: true,
         data: profileData,
+      });
+    } catch (error) {
+      return res.status(400).json({
+        success: false,
+        error: error.message,
+      });
+    }
+  }
+
+  async editPreSubmissionData(req, res) {
+    try {
+      const { userId } = req.params;
+      const updateData = req.body;
+
+      const updatedData = await editPreSubmissionDataUseCase(
+        userId,
+        updateData
+      );
+
+      return res.status(200).json({
+        sucess: true,
+        data: updatedData,
       });
     } catch (error) {
       return res.status(400).json({
