735890d3557c5f23bdb0455bec6f4b521c5c6393
eduardo-hdez <ehdez2004@gmail.com>
2026-09-10 23:32:22 -0600

    feat(r-01): add checkPreSubmissionData use case

---
+++ Domain/UseCases/checkPreSubmissionData.usecase.js
@@ -0,0 +1,26 @@
+import { supabase } from '../../Data/Config/supabase.js';
+import UserModel from '../../Data/Models/user.model.js';
+
+class CheckPreSubmissionDataUseCase {
+  async execute(userId) {
+    const { data, error } = await supabase
+      .from('User')
+      .select(
+        `
+        userID, name, email, birthDate, phone,
+        Address (country, state, city),
+        UserDocuments (ine, proofOfAddress)
+      `
+      )
+      .eq('userID', userId)
+      .single();
+
+    if (error) {
+      throw new Error(error.message);
+    }
+
+    return new UserModel(data);
+  }
+}
+
+export default CheckPreSubmissionDataUseCase;
