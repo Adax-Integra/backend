bf6f00eeba63d4d9d9e744deee1ad5aea06bc2d6
eduardo-hdez <ehdez2004@gmail.com>
2026-09-10 22:56:05 -0600

    feat(r-01): add user model

---
+++ Data/Models/user.model.js
@@ -0,0 +1,14 @@
+class UserModel {
+  constructor(userData) {
+    this.userId = userData.userID;
+    this.name = userData.name;
+    this.email = userData.email;
+    this.password = userData.password;
+    this.officeId = userData.officeID;
+    this.birthDate = userData.birthDate;
+    this.phone = userData.phone;
+    this.createdAt = userData.createdAt;
+  }
+}
+
+export default UserModel;
