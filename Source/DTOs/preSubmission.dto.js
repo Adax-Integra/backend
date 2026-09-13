a92e5ac5cda4a8a1c56db2fe2b2b683fff082326
eduardo-hdez <ehdez2004@gmail.com>
2026-09-13 16:02:20 -0600

    feat(r-01): add externalUserDocuments to preSubmission dto

--- Source/DTOs/preSubmission.dto.js
+++ Source/DTOs/preSubmission.dto.js
@@ -1,13 +1,15 @@
 class PreSubmissionDTO {
-  constructor({ profile, address }) {
+  constructor({ profile, address, documents }) {
     this.profile = profile ?? null;
     this.address = address ?? null;
+    this.documents = documents ?? null;
   }
 
   toJSON() {
     return {
       profile: this.profile,
       address: this.address,
+      documents: this.documents,
     };
   }
 }
