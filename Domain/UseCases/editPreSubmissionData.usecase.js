26ea5ee857aedbef46587a679ca0f41abcf0b459
eduardo-hdez <ehdez2004@gmail.com>
2026-09-13 19:35:30 -0600

    feat(r-01): update documents in edit preSubmission use case

--- Domain/UseCases/editPreSubmissionData.usecase.js
+++ Domain/UseCases/editPreSubmissionData.usecase.js
@@ -1,12 +1,38 @@
 import ExternalUserModel from '../../Data/Models/externalUser.model.js';
+import ExternalUserDocumentsModel from '../../Data/Models/externalUserDocuments.model.js';
 import EditPreSubmissionValidator from '../../Data/Validators/editPreSubmission.validator.js';
 
 class EditPreSubmissionDataUseCase {
   async execute(userId, updateData) {
     const validUserId = EditPreSubmissionValidator.validateUserId(userId);
-    const payload = EditPreSubmissionValidator.validateUpdateBody(updateData);
+    const { profile, documents } =
+      EditPreSubmissionValidator.validateUpdateBody(updateData, validUserId);
 
-    return ExternalUserModel.updateProfile(validUserId, payload);
+    const result = {
+      profile: null,
+      documents: null,
+    };
+
+    if (Object.keys(profile).length > 0) {
+      result.profile = await ExternalUserModel.updateProfile(
+        validUserId,
+        profile
+      );
+    } else {
+      result.profile = await ExternalUserModel.findProfileById(validUserId);
+    }
+
+    if (Object.keys(documents).length > 0) {
+      result.documents = await ExternalUserDocumentsModel.updateByUserId(
+        validUserId,
+        documents
+      );
+    } else {
+      result.documents =
+        await ExternalUserDocumentsModel.findByUserId(validUserId);
+    }
+
+    return result;
   }
 }
 
