74f7acabc7210738b7fb8c953e4d77fe03f906a3
Joel <joeleoj2006@gmail.com>
2026-09-20 21:04:25 -0600

    chore(V-10): Add connection with supabase to allow SQL queries as well as PostgREST

--- Data/Models/cases.model.js
+++ Data/Models/cases.model.js
@@ -1,1 +1,13 @@
-// Here will be the model for all related to cases
+import sql from '../Config/db.js';
+
+class CasesModel {
+  static async getAllCasesFromUser(userId) {
+    const rows = await sql`
+      SQLCODE ${userId}
+    `;
+
+    return rows[0] || null;
+  }
+}
+
+export default CasesModel;
