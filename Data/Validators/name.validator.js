19f5248039a7999b086066342cb0553665c974fc
eduardo-hdez <ehdez2004@gmail.com>
2026-09-22 18:06:36 -0600

    refactor(field-validator): move string validator to own file

--- Data/Validators/name.validator.js
+++ Data/Validators/name.validator.js
@@ -1,13 +1,11 @@
-function validationError(message) {
-  return new Error(message);
-}
+import RequiredStringValidator from './requiredString.validator.js';
 
 class NameValidator {
   static validateNameOrLastName(value) {
-    if (typeof value !== 'string' || value.trim() === '') {
-      throw validationError('name or last name must be a non-empty string.');
-    }
-    return value.trim();
+    return RequiredStringValidator.validateRequiredString(
+      value,
+      'name or last name'
+    );
   }
 }
 
